872c4330d0e90795ba144e4715f9b86f64dc1b2f
Derrick Douglass <contact@derrickdouglassdesigns.com>
2014-10-21 16:36:28 -0400

    original file

--- scenescream.js
+++ scenescream.js
@@ -1,9 +1,9 @@
 /*
 The MIT License (MIT)
-      
+    Changed  
 Copyright (c) 2014 Derrick Douglass
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
+ 
+Permissions is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
@@ -24,55 +24,52 @@ SOFTWARE.
 
 $(document).ready(function(){
 
-   var scenes = {}
+	var scenes = {}
 
     $(document).scroll(function(){
 
-	     // Loop through every element that has a SCENES class to get the Top and Bottom positions of our SCENES in relation to the viewport 
+	     // Loop through every element that has a SCENES class to get the Top and Height positions of our SCENES in relation to the viewport 
     $('.scenes').each(function(){
 		
 		var id = $(this).attr("id");
 		
-			// Get the top, right, bottom, and left offset coordinates of our SCENES and assign them to the variable OBJ 
+			// Get the top, right, Height, and left offset coordinates of our SCENES and assign them to the variable OBJ 
 		var obj = document.getElementById(id).getBoundingClientRect();
 		
 			// Place the Top postions of our SCENES into the SCENES array 
        scenes[this.id + "Top"] = obj.top;  
 	   
-	   		// Place the Bottom postions of our SCENES into the SCENES array 
-	   scenes[this.id + "Bottom"] = 0 - obj.height; //Change .height to .innerHeight, .outerHeight, outerHeight(true); 
+	   		// Place the Height postions of our SCENES into the SCENES array 
+	   scenes[this.id + "Height"] = 0 - obj.height; //Change .height to .innerHeight, .outerHeight, outerHeight(true); 
 	   
 	});
-				// If the Top of a SCENE is above the viewport and the Bottom of a SCENE is below the viewport, we'll do something						 
-            if ( scenes["scene1Top"] <= 0 && scenes["scene1Top"] >= scenes["scene1Bottom"] ) {
-			$(".sceneIndicatorBox")
-			.text("scene 1")
-			.css("background-color","#F90")
-			.css("box-shadow","4px 4px 7px #fff");
-							} 
-			if ( scenes["scene2Top"] <= 0 && scenes["scene2Top"] >= scenes["scene2Bottom"] ) {
-			$(".sceneIndicatorBox")
-			.text("scene 2")
-			.css("background-color","#fff")
-			.css("box-shadow","4px 4px 7px #0f3");
+				// If the Top of a SCENE is above the viewport and the Height of a SCENE is below the viewport, we'll do something						 
+            if ( scenes["scene1Top"] <= 0 && scenes["scene1Top"] >= scenes["scene1Height"] ) {
+				
+				//Add your code here to do something when scene1 is in view
+
+			} 
+			if ( scenes["scene2Top"] <= 0 && scenes["scene2Top"] >= scenes["scene2Height"] ) {
+				
+				//Add your code here to do something when scene2 is in view
+
 			}
-			if ( scenes["scene3Top"] <= 0 && scenes["scene3Top"] >= scenes["scene3Bottom"] ) {
-			$(".sceneIndicatorBox")
-			.text("scene 3")
-			.css("background-color","#0f3")
-			.css("box-shadow","4px 4px 7px #069");
+			if ( scenes["scene3Top"] <= 0 && scenes["scene3Top"] >= scenes["scene3Height"] ) {
+				
+				//Add your code here to do something when scene3 is in view
+
 			}
-			if ( scenes["scene4Top"] <= 0 && scenes["scene4Top"] >= scenes["scene4Bottom"] ) {
-			$(".sceneIndicatorBox")
-			.text("scene 4")
-			.css("background-color","#069")
-			.css("box-shadow","4px 4px 7px #f6c");
+			if ( scenes["scene4Top"] <= 0 && scenes["scene4Top"] >= scenes["scene4Height"] ) {
+				
+				//Add your code here to do something when scene4 is in view
+
 			}
-			if ( scenes["scene5Top"] <= 0 && scenes["scene5Top"] >= scenes["scene5Bottom"] ) {
-			$(".sceneIndicatorBox")
-			.text("scene 5")
-			.css("background-color","#f6c")
-			.css("box-shadow","4px 4px 7px #9cc");
+			if ( scenes["scene5Top"] <= 0 && scenes["scene5Top"] >= scenes["scene5Height"] ) {
+				
+				//Add your code here to do something when scene5 is in view
+
 			}
+			
+			//You can copy and add as many additional scene subroutines you need for your project
     });
 });
